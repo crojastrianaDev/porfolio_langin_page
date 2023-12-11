7678ce0e9b89ac5b571f534e38fd702658e54361
crojastrianDev <crojastriana3@gmail.com>
2023-12-11 10:53:42 -0500

    first commit footer and header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './component/layout/layout.component';
+import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+  },
+];
